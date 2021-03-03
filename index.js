c10002f10b707deabfcd5800e0578a16584fd35e
xaviguasch <xaviguasch@gmail.com>
2021-03-03 19:22:25 +0100

    added solution to the challenge

--- index.js
+++ index.js
@@ -32,4 +32,16 @@ GOOD LUCK 😀
 document.body.append(document.createElement('textarea'))
 document.body.append(document.createElement('button'))
 
-const text = document.querySelector('textarea').value
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value
+
+  const rows = text.split('\n')
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_')
+    // console.log(`${first}${second[0].toUpperCase() + second.slice(1)}`)
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`)
+  }
+})
